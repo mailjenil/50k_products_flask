d067b80c68cbdb75f2bdf30179e032fcf250d0be
Shah <jenil@amazon.com>
2019-07-18 22:55:33 -0700

    AutoComplete complete

--- main.py
+++ main.py
@@ -3,14 +3,17 @@ from flask import request, jsonify
 import json
 from src.product_container_builder import ProductContainerBuilder
 from src.product import ProductTypes
+from src.AutoComplete import AutoComplete
 
 app = flask.Flask(__name__)
 product_path = "data/sample_product_data.tsv"
 
 product_container_builder = ProductContainerBuilder()
 product_container = product_container_builder.build_container(product_path)
-
 products = product_container.get_products()
+
+auto_complete_index_server = AutoComplete()
+auto_complete_index_server.build(products)
 
 
 @app.route('/', methods=['GET'])
@@ -38,9 +41,9 @@ def search_engine_api():
 
 @app.route('/api/products/autocomplete', methods=['POST'])
 def auto_complete_api():
-    request_json = json.loads(request.data)
-    if 'type' in request_json and request_json['type'] in ProductTypes.__members__:
-        # Search Trie
+    request_dict = json.loads(request.data)
+    if 'type' in request_dict and request_dict['type'] in ProductTypes.__members__:
+        return jsonify(auto_complete_index_server.search(request_dict))
     else:
         return jsonify({})
 
